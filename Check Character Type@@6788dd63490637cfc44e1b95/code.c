dbeedcededda60a236c5cffb4bc6cbefffe60f17
Anushka Juneja <anushka0936.becse24@chitkara.edu.in>
2025-02-09 13:02:04 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -1,21 +1,40 @@
 #include<stdio.h>
 
+// int main() {
+//     char ch;
+//     printf("");
+//     scanf("%c", &ch);
+//     if (ch >= '0' && ch <= '9') {
+//         printf("Digit \n");
+//     }
+//     else if (ch >= 'a' && ch <= 'z') {
+//         if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+//             printf("Vowel \n");
+//         else
+//             printf("Consonant \n");
+//         else {
+//             printf("Special Character");
+//         }
+//     }
+//         }
+//     return 0;
+// }
+
 int main() {
     char ch;
     printf("");
     scanf("%c", &ch);
-    if (ch >= '0' && ch <= '9') {
+    if (ch >= 0 && ch <= 9) {
         printf("Digit \n");
     }
-    else if (ch >= 'a' && ch <= 'z') {
-        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-            printf("Vowel \n");
-        else
-            printf("Consonant \n");
-        else {
-            printf("Special Character");
-        }
+    else if (ch >= a && ch <= z) {
+        printf("Special Character \n");
     }
-        }
+    else if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+        printf("Vowel \n");
+    }
+    else {
+        printf("Consonant \n");
+    }
     return 0;
 }
